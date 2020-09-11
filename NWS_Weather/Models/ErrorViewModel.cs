87900c00bbbff26440af7d1b76c2d8d5546020df
Joe Zagorski <j.j.zagorski@gmail.com>
2020-09-11 08:26:06 -0400

    Initial Commit

---
+++ NWS_Weather/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NWS_Weather.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
